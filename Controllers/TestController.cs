f2ac3bb9904b5d2a3dd91c99a049741d7f850bbe
nurnoby admin <nurnoby323@gmail.com>
2025-03-15 11:36:09 +0600

    Contoller and nogetfile done

---
+++ Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT_Auth_Care_Web_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("Index"),Authorize ]
+        public IActionResult Index()
+        {
+            return Ok(new { ff = "This is Private Access"});
+
+        }
+        [HttpGet("QryFromUnauthAre")]
+        public IActionResult QryFromUnauthAre()
+        {
+            return Ok(new { Token = "This is Public Access" });
+        }
+    }
+}
